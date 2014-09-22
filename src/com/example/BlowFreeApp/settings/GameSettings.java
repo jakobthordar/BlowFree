438cd8cda2635ed2a34310c8edf496e554e5e531
alexanderbsig <alexnaderbs.js@gmail.com>
2014-09-22 21:36:07 +0000

    sounds

---
+++ src/com/example/BlowFreeApp/settings/GameSettings.java
@@ -0,0 +1,43 @@
+package com.example.BlowFreeApp.settings;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.widget.CheckBox;
+import android.widget.Toast;
+import com.example.BlowFreeApp.PackLevelFactory;
+import com.example.BlowFreeApp.R;
+import com.example.BlowFreeApp.sound.SoundPlayer;
+
+public class GameSettings extends PreferenceActivity {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+
+        CheckBoxPreference pref = (CheckBoxPreference) findPreference("sound_Theme_sound");
+        pref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+
+                CheckBoxPreference c = (CheckBoxPreference) preference;
+                if ((!c.isChecked())) {
+
+                    PackLevelFactory.getSoundPlayer().getThemePlayer().stop();
+                    return true;
+                }else{
+                    PackLevelFactory.getSoundPlayer().playTheme();
+                }
+
+
+
+                return false;
+            }
+        });
+    }
+}
