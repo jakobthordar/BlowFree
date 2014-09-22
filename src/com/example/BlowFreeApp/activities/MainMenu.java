93cb44bd0e09c771fcb913e57dc4e1796f6e18ed
alexanderbsig <alexnaderbs.js@gmail.com>
2014-09-22 21:35:38 +0000

    sounds

--- src/com/example/BlowFreeApp/activities/MainMenu.java
+++ src/com/example/BlowFreeApp/activities/MainMenu.java
@@ -2,10 +2,14 @@ package com.example.BlowFreeApp.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceManager;
 import android.view.View;
 import com.example.BlowFreeApp.PackLevelFactory;
 import com.example.BlowFreeApp.R;
+import com.example.BlowFreeApp.settings.GameSettings;
 
 
 public class MainMenu extends Activity {
@@ -16,7 +20,11 @@ public class MainMenu extends Activity {
         setContentView(R.layout.activity_main);
         PackLevelFactory packLevelFactory = new PackLevelFactory(this);
 
-        PackLevelFactory.getSoundPlayer().playTheme();
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        if(pref.getBoolean("sound_Theme_sound", true  )){
+            PackLevelFactory.getSoundPlayer().playTheme();
+        }
+
     }
 
     public void newGame(View view){
@@ -29,7 +37,7 @@ public class MainMenu extends Activity {
         startActivity(myIntent);
     }
     public void gameSettings(View view){
-        Intent myIntent = new Intent(this, Settings.class);
+        Intent myIntent = new Intent(this, GameSettings.class);
         startActivity(myIntent);
     }
 }
